b7b08974bbba6b620b86bbc5f7ad332c4d7f5991
Inna Shkilna <shkinai.1995@gmail.com>
2023-04-26 20:12:39 +0300

    25.04

--- js/task-03.js
+++ js/task-03.js
@@ -1,1 +1,23 @@
+const parent = document.querySelector('#parent');
 
+parent.addEventListener('click', event => {
+  console.log('event.target: ', event.target);
+  console.log('event.currentTarget: ', event.currentTarget);
+});
+
+parent.addEventListener('click', () => {
+  alert(
+    'Parent click handler. This alert will not appear when clicking on Descendant, the event will not reach here!'
+  );
+});
+
+child.addEventListener('click', () => {
+  alert(
+    'Child click handler. This alert will not appear when clicking on Descendant, the event will not reach here!'
+  );
+});
+
+descendant.addEventListener('click', event => {
+  event.stopPropagation();
+  alert('Descendant click handler');
+});
--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,68 @@
+/* const mango = {
+  username: 'Mango',
+  showUsername() {
+    console.log(this);
+    console.log(`My username is: ${this.username}`);
+  },
+};
 
+const btn = document.querySelector('.js-btn');
+
+// ✅ Працює
+mango.showUsername();
+
+// ❌ this буде посилатися на button, якщо використовувати showUsername як callback
+btn.addEventListener('click', mango.showUsername); // не працює
+
+// ✅ Не забувайте прив'язувати контекст методів об'єкта
+btn.addEventListener('click', mango.showUsername.bind(mango)); */
+
+/* const button = document.querySelector('.btn');
+
+const handleClick = event => {
+  console.log(event);
+  console.log(event.type);
+};
+
+button.addEventListener('click', handleClick); */
+
+/* document.addEventListener('keydown', event => {
+  console.log('Keydown: ', event);
+});
+
+document.addEventListener('keyup', event => {
+  console.log('Keyup: ', event);
+});
+
+document.addEventListener('keydown', event => {
+  console.log('key: ', event.key);
+  console.log('code: ', event.code);
+}); */
+
+/* document.addEventListener('keydown', event => {
+  event.preventDefault();
+
+  if ((event.ctrlKey || event.metaKey) && event.code === 'KeyS') {
+    console.log('«Ctrl + s» or «Command + s» combo');
+  }
+}); */
+const textInput = document.querySelector('.text-input');
+
+const setFocusBtn = document.querySelector('[data-action="set"]');
+const removeFocusBtn = document.querySelector('[data-action="remove"]');
+
+setFocusBtn.addEventListener('click', () => {
+  textInput.focus();
+});
+
+removeFocusBtn.addEventListener('click', () => {
+  textInput.blur();
+});
+
+textInput.addEventListener('focus', () => {
+  textInput.value = 'This input has focus';
+});
+
+textInput.addEventListener('blur', () => {
+  textInput.value = '';
+});
